b4d217a2d6a15ebae9f4b2d73a192b676865f337
Thien Ngan Doan <ngandn18@gmail.com>
2022-06-13 13:37:02 -0500

    Udacity plotting wrapper
    Udacity plotting wrapper

--- .ipynb_checkpoints/visual-checkpoint.py
+++ .ipynb_checkpoints/visual-checkpoint.py
@@ -1,8 +1,11 @@
-# from matplotlib.pyplot import hist, hist2d
-# from plotting import *
 from seaborn import histplot
+import matplotlib.pyplot as plt
+
 data = [3,4,2,4,3,5,3,6,4,3]
 histplot(data)
+# plt.hist(data, bins=5)
 
+plt.show()
+# matplotlib.pyplot.show()
 
 
--- visual.py
+++ visual.py
@@ -2,8 +2,8 @@ from seaborn import histplot
 import matplotlib.pyplot as plt
 
 data = [3,4,2,4,3,5,3,6,4,3]
-# histplot(data)
-plt.hist(data, bins=5)
+histplot(data)
+# plt.hist(data, bins=5)
 
 plt.show()
 # matplotlib.pyplot.show()
