d635d46064b61747f1e67934558a87d6518658cb
Thien Ngan Doan <ngandn18@gmail.com>
2022-06-13 13:26:47 -0500

    Plotting
    Plotting for basic statistics

---
+++ .ipynb_checkpoints/visual-checkpoint.py
@@ -0,0 +1,8 @@
+# from matplotlib.pyplot import hist, hist2d
+# from plotting import *
+from seaborn import histplot
+data = [3,4,2,4,3,5,3,6,4,3]
+histplot(data)
+
+
+
---
+++ visual.py
@@ -0,0 +1,11 @@
+from seaborn import histplot
+import matplotlib.pyplot as plt
+
+data = [3,4,2,4,3,5,3,6,4,3]
+# histplot(data)
+plt.hist(data, bins=5)
+
+plt.show()
+# matplotlib.pyplot.show()
+
+
